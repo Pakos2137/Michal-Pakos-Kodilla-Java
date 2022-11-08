40edf9c4657bc82b94775a14227c6727dea65dff
Michal Pakos <michal.pakos@onet.pl>
2022-11-09 00:46:46 +0100

    First commit

--- src/main/java/com/kodilla/Calculator.java
+++ src/main/java/com/kodilla/Calculator.java
@@ -4,10 +4,20 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class KodillaCourseApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(KodillaCourseApplication.class, args);
+public class Calculator {
+    int a = 8;
+    int b = 9;
+    public void add() {
+        int c = a + b;
+        System.out.println(a + "+" + b + "=" + c);
+    }
+    public void subtract() {
+        int c = a - b;
+        System.out.println(a + "-" + b + "=" + c);
     }
 
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        calculator.subtract();
+    }
 }
