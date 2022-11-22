7a68cd1bf1d7483f0e213a11b7c0e3d6ba573be4
Michal Pakos <michal.pakos@onet.pl>
2022-11-22 21:58:29 +0100

    6.2-Test-Driven-Development

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator.java
@@ -0,0 +1,11 @@
+package com.kodilla.testing;
+
+class Calculator {
+    public static int addAToB(int a, int b) {
+        return a + b;
+    }
+
+    public static int substractAFromB(int a, int b) {
+        return a - b;
+    }
+}
