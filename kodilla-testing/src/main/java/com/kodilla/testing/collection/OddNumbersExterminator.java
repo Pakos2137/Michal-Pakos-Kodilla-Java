4092c1f92fbfdd872603aee1dff25c7a88aa7cfa
Michal Pakos <michal.pakos@onet.pl>
2022-12-05 20:13:12 +0100

    6.3.-JUnit,-czyli-pierwsze-testy

--- kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
@@ -1,9 +1,16 @@
 package com.kodilla.testing.collection;
 
-
-import java.util.List;
+import java.util.*;
 
 public class OddNumbersExterminator {
-    public List<Integer> exterminate(List<Integer> numbers)
+    public List<Integer> exterminate(List<Integer> numbers) {
+        ArrayList<Integer>oddNumberList = new ArrayList<>();
 
+        for(int number : numbers) {
+            if (number % 2 == 0) {
+                oddNumberList.add(number);
+            }
+        }
+        return oddNumberList;
+    }
 }
--- kodilla-testing/src/test/java/com/kodilla/testing/collection/ForumTestSuite.java
+++
@@ -1,8 +0,0 @@
-package com.kodilla.testing.collection;
-import com.kodilla.testing.user.SimpleUser;
-
-import org.junit.jupiter.api.*;
-
-class ForumTestSuite {
-
-}
