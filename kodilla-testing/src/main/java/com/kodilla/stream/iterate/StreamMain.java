7e33a72ef5be3a8569ff4bf6ee3c88352eac150f
Michal Pakos <michal.pakos@onet.pl>
2022-12-07 21:43:19 +0100

    7.2-Stream

---
+++ kodilla-testing/src/main/java/com/kodilla/stream/iterate/StreamMain.java
@@ -0,0 +1,9 @@
+package com.kodilla.stream.iterate;
+
+class Application {
+
+    public static void main(String[] args) {
+        ForumUser forumUser = new ForumUser("Pakos2137", "Michal Pakos");
+        System.out.println(forumUser);
+    }
+}
