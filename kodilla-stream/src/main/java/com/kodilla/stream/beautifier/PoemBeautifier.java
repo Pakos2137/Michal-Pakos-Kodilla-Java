6d4f5afa10f03425081be0c2be3c81c711315c00
Michal Pakos <michal.pakos@onet.pl>
2022-12-07 16:36:55 +0100

    commit test

--- kodilla-stream/src/main/java/com/kodilla/stream/beautifier/PoemBeautifier.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/beautifier/PoemBeautifier.java
@@ -2,6 +2,6 @@ package com.kodilla.stream.beautifier;
 
 public class PoemBeautifier {
     public void beautify(String textToBeautify, PoemDecorator decorator) {
-        System.out.println(decorator.decorate(textToBeautify));
+        System.out.println(decorator.decorate(textToBeautify)); //komentarz
     }
 }
