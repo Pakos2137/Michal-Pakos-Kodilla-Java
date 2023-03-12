cf4bd7e53d870b4e5520cccc475b4904e604eb4c
Pakos <michal.pakos@onet.pl>
2023-03-12 19:03:35 +0100

    15.6

---
+++ kodilla-testing/src/test/java/com/kodilla/testing/loop/testLoopAndConditionalBreakpoints.java
@@ -0,0 +1,18 @@
+package com.kodilla.testing.loop;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.Assert.*;
+
+public class testLoopAndConditionalBreakpoints {
+    @Test
+    public void testLoop() {
+        long sum = 0;
+
+        for(int n = 0; n < 1000;n++) {
+            sum += n;
+
+            System.out.println(sum);
+        }
+        assertEquals(499500,sum);
+    }
+}
