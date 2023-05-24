c46b326ba049eec6a38a25046fb039256b95d698
Pakos2137 <michal.pakos@onet.pl>
2023-05-24 23:26:49 +0200

    24.5

---
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/adapter/bookclassifier/MedianAdaptee.java
@@ -0,0 +1,19 @@
+package com.kodilla.patterns2.adapter.bookclassifier;
+
+import com.kodilla.patterns2.adapter.bookclassifier.libraryb.Book;
+import com.kodilla.patterns2.adapter.bookclassifier.libraryb.BookSignature;
+import com.kodilla.patterns2.adapter.bookclassifier.libraryb.BookStatistics;
+
+import java.util.Map;
+
+public class MedianAdaptee implements BookStatistics {
+    @Override
+    public int averagePublicationYear(Map<BookSignature, Book> books) {
+        return 0;
+    }
+
+    @Override
+    public int medianPublicationYear(Map<BookSignature, Book> books) {
+        return 0;
+    }
+}
