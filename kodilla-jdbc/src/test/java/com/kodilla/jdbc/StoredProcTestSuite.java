390d21a4a51522717c8370c4f1bb3cb4a852de4a
Pakos2137 <michal.pakos@onet.pl>
2023-05-30 21:49:24 +0200

    30.3

--- kodilla-jdbc/src/test/java/com/kodilla/jdbc/StoredProcTestSuite.java
+++ kodilla-jdbc/src/test/java/com/kodilla/jdbc/StoredProcTestSuite.java
@@ -13,12 +13,13 @@ public class StoredProcTestSuite {
     @Test
     void testUpdateBestsellers() throws SQLException {
 
+        int howMany = 0;
+
         DbManager dbManager = DbManager.getInstance();
 
         String sqlUpdate = "UPDATE BOOKS SET BESTSELLER = false";
 
         Statement statement = dbManager.getConnection().createStatement();
-
         statement.executeUpdate(sqlUpdate);
 
         String sqlCheckTable = "SELECT COUNT(*) AS HOW_MANY FROM BOOKS WHERE BESTSELLER = true";
@@ -29,12 +30,12 @@ public class StoredProcTestSuite {
         statement2.execute(sqlProcedureCall);
         ResultSet rs = statement.executeQuery(sqlCheckTable);
 
-        int howMany = -1;
         if (rs.next()) {
             howMany = rs.getInt("HOW_MANY");
         }
-        assertEquals(3, howMany);
+        assertEquals(2, howMany);
         rs.close();
+
         statement.close();
         statement2.close();
     }
